d86709d0ade108be4537dc2dd491fc3005c27c8d
ETumang <emily.tumang@gmail.com>
2015-02-06 00:45:29 -0500

    So many dictionaries.

---
+++ data.py
@@ -0,0 +1,518 @@
+dict districts = {
+	'kai':kailahun,
+	'ken':kenema,
+	'kon':kono,
+	'bom':bombali,
+	'kam':kambia,
+	'koi':koinadugu,
+	'por':port_loko,
+	'ton':tonkolili,
+	'bod':bo,
+	'bon':bonthe,
+	'moy':moyamba,
+	'puj':pujehun,
+	'wer':western_rural,
+	'weu':western_urban	
+}
+
+dict notes{
+	"population":"number of people in the district",
+	"infected":"percent of people with n risk of death that day"
+	"vaccinated":"percent of people at that risk level",
+	"workers":"# of people actively vaccinating others",
+	"resistance":"probability that someone will refuse vaccination",
+	"education":"awareness of the drug and its effects",
+	"supply": "# of doses available for that district"
+}
+
+dict kailahun{
+	'abbrev':'kai',
+	'population':358190,
+	'infected': kai_inf,
+	'vaccinated':kai_vac,
+	'workers':1,
+	'resistance':.25,
+	'education':.1,
+	'supply':4000
+}
+
+dict kai_inf{
+	'100-90':0,
+	'90-80':0,
+	'80-70':0,
+	'70-60':0,
+	'60-50':0,
+	'40-30':0,
+	'30-20':0,
+	'20-10':0,
+	'10-0':0,
+}
+
+dict kai_vac{
+	'100-90':0,
+	'90-80':0,
+	'80-70':0,
+	'70-60':0,
+	'60-50':0,
+	'40-30':0,
+	'30-20':0,
+	'20-10':0,
+	'10-0':0,
+}
+
+dict kenema{
+	'abbrev':'ken',
+	'population':497948,
+	'infected': ken_inf,
+	'vaccinated':ken_vac,
+	'workers':1,
+	'resistance':.2,
+	'education':.2,
+	'supply':2000
+}
+
+dict ken_inf{
+	'100-90':0,
+	'90-80':0,
+	'80-70':0,
+	'70-60':0,
+	'60-50':0,
+	'40-30':0,
+	'30-20':0,
+	'20-10':0,
+	'10-0':0,
+}
+
+dict ken_vac{
+	'100-90':0,
+	'90-80':0,
+	'80-70':0,
+	'70-60':0,
+	'60-50':0,
+	'40-30':0,
+	'30-20':0,
+	'20-10':0,
+	'10-0':0,
+}
+
+dict kono{
+	'abbrev':'kon',
+	'population':497948,
+	'infected': kon_inf,
+	'vaccinated':kon_vac,
+	'workers':1,
+	'resistance':.2,
+	'education':.2,
+	'supply':2000
+}
+
+dict kon_inf{
+	'100-90':0,
+	'90-80':0,
+	'80-70':0,
+	'70-60':0,
+	'60-50':0,
+	'40-30':0,
+	'30-20':0,
+	'20-10':0,
+	'10-0':0,
+}
+
+dict kon_vac{
+	'100-90':0,
+	'90-80':0,
+	'80-70':0,
+	'70-60':0,
+	'60-50':0,
+	'40-30':0,
+	'30-20':0,
+	'20-10':0,
+	'10-0':0,
+}
+
+dict bombali{
+	'abbrev':'bom',
+	'population':408390,
+	'infected': bom_inf,
+	'vaccinated':bom_vac,
+	'workers':1,
+	'resistance':.2,
+	'education':.2,
+	'supply':2000
+}
+
+dict bom_inf{
+	'100-90':0,
+	'90-80':0,
+	'80-70':0,
+	'70-60':0,
+	'60-50':0,
+	'40-30':0,
+	'30-20':0,
+	'20-10':0,
+	'10-0':0,
+}
+
+dict bom_vac{
+	'100-90':0,
+	'90-80':0,
+	'80-70':0,
+	'70-60':0,
+	'60-50':0,
+	'40-30':0,
+	'30-20':0,
+	'20-10':0,
+	'10-0':0,
+}
+
+dict kambia{
+	'abbrev':'kam',
+	'population':497948,
+	'infected': kam_inf,
+	'vaccinated':kam_vac,
+	'workers':1,
+	'resistance':.2,
+	'education':.2,
+	'supply':2000
+}
+
+dict kam_inf{
+	'100-90':0,
+	'90-80':0,
+	'80-70':0,
+	'70-60':0,
+	'60-50':0,
+	'40-30':0,
+	'30-20':0,
+	'20-10':0,
+	'10-0':0,
+}
+
+dict kam_vac{
+	'100-90':0,
+	'90-80':0,
+	'80-70':0,
+	'70-60':0,
+	'60-50':0,
+	'40-30':0,
+	'30-20':0,
+	'20-10':0,
+	'10-0':0,
+}
+
+dict koinadugu{
+	'abbrev':'koi',
+	'population':265758,
+	'infected': koi_inf,
+	'vaccinated':koi_vac,
+	'workers':1,
+	'resistance':.2,
+	'education':.2,
+	'supply':2000
+}
+
+dict koi_inf{
+	'100-90':0,
+	'90-80':0,
+	'80-70':0,
+	'70-60':0,
+	'60-50':0,
+	'40-30':0,
+	'30-20':0,
+	'20-10':0,
+	'10-0':0,
+}
+
+dict koi_vac{
+	'100-90':0,
+	'90-80':0,
+	'80-70':0,
+	'70-60':0,
+	'60-50':0,
+	'40-30':0,
+	'30-20':0,
+	'20-10':0,
+	'10-0':0,
+}
+
+dict port_loko{
+	'abbrev':'por',
+	'population':497948,
+	'infected': por_inf,
+	'vaccinated':por_vac,
+	'workers':1,
+	'resistance':.2,
+	'education':.2,
+	'supply':2000
+}
+
+dict por_inf{
+	'100-90':0,
+	'90-80':0,
+	'80-70':0,
+	'70-60':0,
+	'60-50':0,
+	'40-30':0,
+	'30-20':0,
+	'20-10':0,
+	'10-0':0,
+}
+
+dict por_vac{
+	'100-90':0,
+	'90-80':0,
+	'80-70':0,
+	'70-60':0,
+	'60-50':0,
+	'40-30':0,
+	'30-20':0,
+	'20-10':0,
+	'10-0':0,
+}
+
+dict tonkolili{
+	'abbrev':'ton',
+	'population':347197,
+	'infected': kam_inf,
+	'vaccinated':kam_vac,
+	'workers':1,
+	'resistance':.2,
+	'education':.2,
+	'supply':2000
+}
+
+dict ton_inf{
+	'100-90':0,
+	'90-80':0,
+	'80-70':0,
+	'70-60':0,
+	'60-50':0,
+	'40-30':0,
+	'30-20':0,
+	'20-10':0,
+	'10-0':0,
+}
+
+dict ton_vac{
+	'100-90':0,
+	'90-80':0,
+	'80-70':0,
+	'70-60':0,
+	'60-50':0,
+	'40-30':0,
+	'30-20':0,
+	'20-10':0,
+	'10-0':0,
+}
+
+dict bo{
+	'abbrev':'bod',
+	'population':463668,
+	'infected': bod_inf,
+	'vaccinated': bod_vac,
+	'workers':1,
+	'resistance':.2,
+	'education':.2,
+	'supply':2000
+}
+
+dict bod_inf{
+	'100-90':0,
+	'90-80':0,
+	'80-70':0,
+	'70-60':0,
+	'60-50':0,
+	'40-30':0,
+	'30-20':0,
+	'20-10':0,
+	'10-0':0,
+}
+
+dict bod_vac{
+	'100-90':0,
+	'90-80':0,
+	'80-70':0,
+	'70-60':0,
+	'60-50':0,
+	'40-30':0,
+	'30-20':0,
+	'20-10':0,
+	'10-0':0,
+}
+
+dict bonthe{
+	'abbrev':'bon',
+	'population':139687,
+	'infected': bon_inf,
+	'vaccinated': bon_vac,
+	'workers':1,
+	'resistance':.2,
+	'education':.2,
+	'supply':2000
+}
+
+dict bon_inf{
+	'100-90':0,
+	'90-80':0,
+	'80-70':0,
+	'70-60':0,
+	'60-50':0,
+	'40-30':0,
+	'30-20':0,
+	'20-10':0,
+	'10-0':0,
+}
+
+dict bon_vac{
+	'100-90':0,
+	'90-80':0,
+	'80-70':0,
+	'70-60':0,
+	'60-50':0,
+	'40-30':0,
+	'30-20':0,
+	'20-10':0,
+	'10-0':0,
+}
+
+dict moyamba{
+	'abbrev':'moy',
+	'population':260190,
+	'infected': moy_inf,
+	'vaccinated': moy_vac,
+	'workers':1,
+	'resistance':.2,
+	'education':.2,
+	'supply':2000
+}
+
+dict moy_inf{
+	'100-90':0,
+	'90-80':0,
+	'80-70':0,
+	'70-60':0,
+	'60-50':0,
+	'40-30':0,
+	'30-20':0,
+	'20-10':0,
+	'10-0':0,
+}
+
+dict moy_vac{
+	'100-90':0,
+	'90-80':0,
+	'80-70':0,
+	'70-60':0,
+	'60-50':0,
+	'40-30':0,
+	'30-20':0,
+	'20-10':0,
+	'10-0':0,
+}
+
+dict pujehun{
+	'abbrev':'puj',
+	'population':228392,
+	'infected': puj_inf,
+	'vaccinated': puj_vac,
+	'workers':1,
+	'resistance':.2,
+	'education':.2,
+	'supply':2000
+}
+
+dict puj_inf{
+	'100-90':0,
+	'90-80':0,
+	'80-70':0,
+	'70-60':0,
+	'60-50':0,
+	'40-30':0,
+	'30-20':0,
+	'20-10':0,
+	'10-0':0,
+}
+
+dict puj_vac{
+	'100-90':0,
+	'90-80':0,
+	'80-70':0,
+	'70-60':0,
+	'60-50':0,
+	'40-30':0,
+	'30-20':0,
+	'20-10':0,
+	'10-0':0,
+}
+
+dict western_rural{
+	'abbrev':'wer',
+	'population':174249,
+	'infected': wur_inf,
+	'vaccinated': wur_vac,
+	'workers':1,
+	'resistance':.2,
+	'education':.2,
+	'supply':2000
+}
+
+dict wur_inf{
+	'100-90':0,
+	'90-80':0,
+	'80-70':0,
+	'70-60':0,
+	'60-50':0,
+	'40-30':0,
+	'30-20':0,
+	'20-10':0,
+	'10-0':0,
+}
+
+dict wur_vac{
+	'100-90':0,
+	'90-80':0,
+	'80-70':0,
+	'70-60':0,
+	'60-50':0,
+	'40-30':0,
+	'30-20':0,
+	'20-10':0,
+	'10-0':0,
+}
+
+dict western_urban{
+	'abbrev':'weu',
+	'population':772873,
+	'infected': weu_inf,
+	'vaccinated': weu_vac,
+	'workers':1,
+	'resistance':.2,
+	'education':.2,
+	'supply':2000
+}
+
+dict weu_inf{
+	'100-90':0,
+	'90-80':0,
+	'80-70':0,
+	'70-60':0,
+	'60-50':0,
+	'40-30':0,
+	'30-20':0,
+	'20-10':0,
+	'10-0':0,
+}
+
+dict weu_vac{
+	'100-90':0,
+	'90-80':0,
+	'80-70':0,
+	'70-60':0,
+	'60-50':0,
+	'40-30':0,
+	'30-20':0,
+	'20-10':0,
+	'10-0':0,
+}
+
+
