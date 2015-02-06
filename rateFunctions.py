8598f510c1910348d4cc7552ff15d88e813e5c59
Halie Murray-Davis <hmurraydavis@yahoo.com>
2015-02-06 07:05:32 -0500

    Add soem function defs and documentation

---
+++ rateFunctions.py
@@ -0,0 +1,34 @@
+#import the massive district dictionary from Emily's Magick
+districts = {}
+
+def supply(districts):
+    '''Calculate the effect of vacination supply on:
+        1. innoculation'''
+        
+        
+def innoculation(districts):
+    '''Calculate the effect of innoculation on:
+        1. # of workers
+        2. infection 
+    '''
+    
+def workers(districts):
+    '''Calculate any changes in the number of WORKERS for a given 
+    district and modify this value in the dictionary accordingly. '''
+    
+def resistance(districts):
+    '''Calculate the resistance of people in a given district to 
+    receiving the drug and modify this value in the dictionary'''
+    
+    
+def education(districts):
+    '''Calculate the effect of education on:
+        1. RESISTANCE to being vacinated (less resistance), and
+        2. VACINATION (increase in # of shots)
+    Also audjusts the value of education for the number of workers
+    in a district (directly related)'''
+    
+    
+def infection(districts): 
+    
+    
