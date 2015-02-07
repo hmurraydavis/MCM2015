b7aefca745cf1ea82800756f2f1cb5ca45a9c819
Halie Murray-Davis <hmurraydavis@yahoo.com>
2015-02-07 04:11:48 -0500

    wtf? Immutable lists? Hlp!!!

--- smallWorld.py
+++ smallWorld.py
@@ -1,7 +1,7 @@
-graph = [ {'gender':0, #0 is female, 1 is male
+'''graph = [ {'gender':0, #0 is female, 1 is male
     'inContact':[[n1,weightn1],[n2,weightn2],[n3,weightn3]], #list of the people they're connected with --> outer list is the connedted verticies, nested list's 0th element is the connected vertex. 1st elemennt is the weight of that edge
     'demise':'zombie' #state of life-fullness, string, choices: 'dead', 'alive', 'zombie'
-    'natImmunity':30 #Naturally occuring, percent immunity of the person
+    'natImmunity':.30 #Naturally occuring, percent immunity of the person
     'age':20 #integer representing person's age
     'innocFac':[0,0,1,0,0,1,1,0] #list representing the innoculation of the person, 0==not vaccinated on that day, 1==vaccinated that day, index of array is the day
     'ebola':0 #do they have ebola? 0==no, 1==yes
@@ -10,4 +10,18 @@ graph = [ {'gender':0, #0 is female, 1 is male
     },
     {...},
     {...}
-]
+]'''
+
+import pprint
+
+graph = []
+for vertex in range(125):
+    graph[vertex] = 3
+#    graph[vertex] = {'n':vertex,
+#        'ebola':0}#,
+#        'demise':'alive',
+#        'natImmunity':.3,
+#        'innocFac':[0],
+#        'pDeath':0.0} 
+
+pprint.pprint(graph)
