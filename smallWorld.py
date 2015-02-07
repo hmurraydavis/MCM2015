cf0f86b49222918c714f13550343f386c54f2b44
jessoehrlein <jessoehrlein@gmail.com>
2015-02-06 22:54:29 -0500

    Fix Halie's typos

--- smallWorld.py
+++ smallWorld.py
@@ -1,9 +1,9 @@
 graph = [ {'gender':0, #0 is female, 1 is male
-    'inContact':[[n1,weightn1],[n2,weightn2],[n2,weightn3]], #list of the people they're connected with --> outer list is the connedted verticies, nested list's 0th element is the connected vertex. 1st elemennt is the weight of that edge
+    'inContact':[[n1,weightn1],[n2,weightn2],[n3,weightn3]], #list of the people they're connected with --> outer list is the connedted verticies, nested list's 0th element is the connected vertex. 1st elemennt is the weight of that edge
     'demise':'zombie' #state of life-fullness, string, choices: 'dead', 'alive', 'zombie'
-    'natImunity':30 #Naturally occuring, percent imunity of the person
+    'natImmunity':30 #Naturally occuring, percent immunity of the person
     'age':20 #integer representing person's age
-    'innocFac':[0,0,1,0,0,1,1,0] #list representing the innoculation of the person, 0==not vacinated on that day, 1==vacinated that day, index of array is the day
+    'innocFac':[0,0,1,0,0,1,1,0] #list representing the innoculation of the person, 0==not vaccinated on that day, 1==vaccinated that day, index of array is the day
     'ebola':0 #do they have ebola? 0==no, 1==yes
     'pDeath': .4 #probability of person dying that day
     'n':3 #n value of that person, vame as the index of the person in the array
